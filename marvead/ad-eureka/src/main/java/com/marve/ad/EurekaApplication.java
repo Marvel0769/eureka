fedaad16a42acb4d779b5db274c4884e21a46747
wangtianbao <624403412@qq.com>
2019-08-23 16:42:27 +0800

    第三讲

--- marvead/ad-eureka/src/main/java/com/marve/ad/EurekaApplication.java
+++ marvead/ad-eureka/src/main/java/com/marve/ad/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.marve.cn.eureka;
+package com.marve.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+    	SpringApplication.run(EurekaApplication.class, args);
     }
-
 }
